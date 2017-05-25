a57dea149bd8908f086fd1cbc7c3b598dad28c97
sandipghosh <sandipgh19@gmail.com>
2017-05-25 12:11:45 +0530

    fetch image with library

--- app/src/main/java/com/example/sandipghosh/imagelibrary/MainActivity.java
+++ app/src/main/java/com/example/sandipghosh/imagelibrary/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.sandipghosh.imagelibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //Initializing the ImageView
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        //Loading Image from URL
+        Picasso.with(this)
+                .load("https://www.simplifiedcoding.net/wp-content/uploads/2015/10/advertise.png")
+                .placeholder(R.drawable.placeholder)   // optional
+                .error(R.drawable.error)      // optional
+                .resize(400,400)                        // optional
+                .into(imageView);
     }
 }
