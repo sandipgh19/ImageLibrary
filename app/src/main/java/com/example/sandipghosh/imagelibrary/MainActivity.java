a556802c57126210472dcd426dcc3e6a0e55f2df
sandipghosh <sandipgh19@gmail.com>
2017-05-27 21:43:53 +0530

    Display Picture

--- app/src/main/java/com/example/sandipghosh/imagelibrary/MainActivity.java
+++ app/src/main/java/com/example/sandipghosh/imagelibrary/MainActivity.java
@@ -2,29 +2,71 @@ package com.example.sandipghosh.imagelibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imageView;
+    private final String android_version_names[] = {
+            "Donut",
+            "Eclair",
+            "Froyo",
+            "Gingerbread",
+            "Honeycomb",
+            "Ice Cream Sandwich",
+            "Jelly Bean",
+            "KitKat",
+            "Lollipop",
+            "Marshmallow"
+    };
+
+    private final String android_image_urls[] = {
+            "http://api.learn2crack.com/android/images/donut.png",
+            "http://api.learn2crack.com/android/images/eclair.png",
+            "http://api.learn2crack.com/android/images/froyo.png",
+            "http://api.learn2crack.com/android/images/ginger.png",
+            "http://api.learn2crack.com/android/images/honey.png",
+            "http://api.learn2crack.com/android/images/icecream.png",
+            "http://api.learn2crack.com/android/images/jellybean.png",
+            "http://api.learn2crack.com/android/images/kitkat.png",
+            "http://api.learn2crack.com/android/images/lollipop.png",
+            "http://api.learn2crack.com/android/images/marshmallow.png"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initViews();
+    }
 
-        //Initializing the ImageView
-        imageView = (ImageView) findViewById(R.id.imageView);
+    private void initViews(){
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.card_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
 
-        //Loading Image from URL
-        Picasso.with(this)
-                .load("https://www.simplifiedcoding.net/wp-content/uploads/2015/10/advertise.png")
-                .placeholder(R.drawable.placeholder)   // optional
-                .error(R.drawable.error)      // optional
-                .resize(400,400)                        // optional
-                .into(imageView);
+        ArrayList androidVersions = prepareData();
+        DataAdapter adapter = new DataAdapter(getApplicationContext(),androidVersions);
+        recyclerView.setAdapter(adapter);
+
+    }
+    private ArrayList prepareData(){
+
+        ArrayList android_version = new ArrayList<>();
+        for(int i=0;i<android_version_names.length;i++){
+            Card card = new Card();
+            card.setAndroid_version_name(android_version_names[i]);
+            card.setAndroid_image_url(android_image_urls[i]);
+            android_version.add(card);
+        }
+        return android_version;
     }
 }
